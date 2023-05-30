0e00898e04544230850481647c4c378edf439284
Benedikt Wehner <benedikt.wehner@outlook.de>
2023-05-30 22:16:59 +0200

    client game implementation

---
+++ src/client/game.ts
@@ -0,0 +1,92 @@
+import { GameConfigurationData } from "./types";
+
+export default class Game {
+    private readonly BASELINE_MARGIN = 10
+    private readonly BASELINE_WIDTH = 5
+    private readonly N_BASELINE_STRIPS = 15
+    private readonly FOREGROUND = '#ffffff'
+
+    readonly ctx: CanvasRenderingContext2D
+    readonly config: GameConfigurationData
+
+    readonly height: number
+    readonly width: number
+
+    private readonly scoreSize: number
+    private readonly scoreLeftX: number
+    private readonly scoreRightX: number
+
+    private readonly baselineHeight: number
+    private readonly baselineX: number
+
+    readonly rightBlockX: number
+
+    scoreLeft: number = 0
+    scoreRight: number = 0
+
+    leftBlockY: number
+    rightBlockY: number
+
+    ballPosX: number
+    ballPosY: number
+
+    constructor(config: GameConfigurationData, ctx: CanvasRenderingContext2D) {
+        this.config = config
+        this.ctx = ctx
+        this.height = config.height
+        this.width = config.width
+        this.leftBlockY = config.leftBlock.posY
+        this.rightBlockY = config.rightBlock.posY
+        this.rightBlockX = this.width - config.rightBlock.margin - config.rightBlock.width
+
+        this.scoreSize = this.height / 7
+        this.scoreLeftX = this.width / 4 - this.scoreSize / 3
+        this.scoreRightX = this.width * 0.75 - this.scoreSize / 3
+
+        this.baselineHeight = (this.height - (this.N_BASELINE_STRIPS + 1) * this.BASELINE_MARGIN) / this.N_BASELINE_STRIPS
+        this.baselineX = (this.width - this.BASELINE_WIDTH) / 2
+    }
+
+    init(): void {
+        // foreground
+        this.ctx.fillStyle = this.FOREGROUND
+        this.ctx.font = `${this.scoreSize}px sans-serif`;
+        this.renderBaseline()
+    }
+
+    clear(): void {
+        this.ctx.clearRect(0, 0, this.width, this.height)
+    }
+
+    render(): void {
+        this.clear()
+        this.renderBaseline()
+        this.renderBall()
+        this.renderScore()
+        this.renderLeftBlock()
+        this.renderRightBlock()
+    }
+
+    renderBall(): void {
+        this.ctx.fillRect(this.ballPosX, this.ballPosY, this.config.ball.size, this.config.ball.size)
+    }
+
+    renderBaseline(): void {
+        for (let i = 0; i < this.N_BASELINE_STRIPS + 1; i++) {
+            this.ctx.fillRect(this.baselineX, i * this.BASELINE_MARGIN + (i - 1) * this.baselineHeight, this.BASELINE_WIDTH, this.baselineHeight)
+        }
+    }
+
+    renderScore(): void {
+        this.ctx.fillText(this.scoreLeft.toString(), this.scoreLeftX, this.scoreSize)
+        this.ctx.fillText(this.scoreRight.toString(), this.scoreRightX, this.scoreSize)
+    }
+
+    renderLeftBlock(): void {
+        this.ctx.fillRect(this.config.leftBlock.margin, this.leftBlockY, this.config.leftBlock.width, this.config.leftBlock.height)
+    }
+
+    renderRightBlock(): void {
+        this.ctx.fillRect(this.rightBlockX, this.rightBlockY, this.config.rightBlock.width, this.config.rightBlock.height)
+    }
+}
