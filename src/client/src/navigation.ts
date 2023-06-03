12e94d9662ebf674eaed65152bbd70f12eb722a6
Benedikt Wehner <benedikt.wehner@outlook.de>
2023-06-03 12:35:56 +0200

    nav destroy func

--- src/client/src/navigation.ts
+++ src/client/src/navigation.ts
@@ -1,7 +1,10 @@
 import { Page } from "./types"
 
+let current: Page | null = null
+
 export default function load(page: Page){
-    console.log(document.body)
+    current?.destroy()
     document.body.innerHTML = page.render()
     page.onLoaded()
+    current = page
 }
